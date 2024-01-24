a0922fa807ad52cb8d00edb7ebba2f5eeab8cf13
page1597 <62283847+page1597@users.noreply.github.com>
2024-01-24 14:31:02 +0900

    프로젝트 초기 셋업

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./pages/root.tsx";
+import ErrorPage from "./pages/error-page.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
