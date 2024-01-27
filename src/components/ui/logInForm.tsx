d5678941221c6ad371df04ab42ae33a5aee5016b
page1597 <62283847+page1597@users.noreply.github.com>
2024-01-27 15:39:48 +0900

    Feat: 유저 회원가입, 로그인 시 아이디 인풋 삭제

--- src/components/ui/logInForm.tsx
+++ src/components/ui/logInForm.tsx
@@ -4,26 +4,24 @@ import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { logIn } from "@/firebase";
+import { googleLogIn, logIn } from "@/firebase";
 import { NavigateFunction } from "react-router-dom";
 import { LoginSocialGoogle, IResolveParams } from "reactjs-social-login";
 import { useCallback, useState } from "react";
-import { GoogleLoginButton } from "react-social-login-buttons";
+import GoogleLoginButton from "./googleLoginButton";
+// import GoogleLoginButton from "./googleLoginButton";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 const formSchema = z.object({
-  id: z.string().min(2, {
-    message: "id must be at least 2 characters.",
-  }),
-  password: z.string().min(2, {
-    message: "password must be at least 2 characters.",
-  }),
+  email: z.string(),
+  password: z.string(),
 });
-// ^(?!.*(1234|5678|password|admin|qwerty|abcd|12345|98765|55555|11111|66666|00000|4321|8765|user|root|login|letmein|asdfgh|qwertyuiop|zxcvbnm|123qwe)).*(?=(.*[A-Z]){1,})(?=(.*[a-z]){1,})(?=(.*\d){1,})(?=(.*[\W_]){1,})[A-Za-z\d\W_]{10,}$
 export default function LogInForm({ navigate }: { navigate: NavigateFunction }) {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      id: "",
+      email: "",
       password: "",
     },
   });
@@ -34,29 +32,28 @@ export default function LogInForm({ navigate }: { navigate: NavigateFunction }) 
 
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-
     console.log(values);
 
     // const user: { email: string; password: string } = {
     //   email: values.id, // 아이디 입력되면 이메일 가져와서 넣을 수 있게 하기
     //   password: values.password,
     // };
-    logIn(values.id, values.password, navigate);
+    logIn(values.email, values.password, navigate);
   }
 
-  const [provider, setProvider] = useState("");
-  const [profile, setProfile] = useState<any>();
+  // const [provider, setProvider] = useState("");
+  // const [profile, setProfile] = useState<any>();
 
-  const onLoginStart = useCallback(() => {
-    alert("login start");
-  }, []);
+  // const onLoginStart = useCallback(() => {
+  //   alert("login start");
+  // }, []);
 
-  const onLogoutSuccess = useCallback(() => {
-    setProfile(null);
-    setProvider("");
-    alert("logout success");
-  }, []);
-
+  // const onLogoutSuccess = useCallback(() => {
+  //   setProfile(null);
+  //   setProvider("");
+  //   alert("logout success");
+  // }, []);
+  const provider = new GoogleAuthProvider();
   const onLogout = useCallback(() => {}, []);
 
   return (
@@ -64,12 +61,11 @@ export default function LogInForm({ navigate }: { navigate: NavigateFunction }) 
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
         <FormField
           control={form.control}
-          name="id"
+          name="email"
           render={({ field }) => (
             <FormItem>
-              {/* <FormLabel>id</FormLabel> */}
               <FormControl>
-                <Input placeholder="아이디" {...field} />
+                <Input placeholder="이메일" {...field} />
               </FormControl>
               {/* <FormDescription>This is your public display name.</FormDescription> */}
               <FormMessage />
@@ -90,20 +86,24 @@ export default function LogInForm({ navigate }: { navigate: NavigateFunction }) 
         />
         <Button type="submit">로그인</Button>
       </form>
+      {/* <LoginSocialGoogle>
+        <GoogleLoginButton />
+      </LoginSocialGoogle> */}
       {/* <LoginSocialGoogle
         isOnlyGetToken
-        client_id={process.env.REACT_APP_GG_APP_ID || ""}
+        client_id="732913480632-ui7vn8sq17ibsmnjasmnpke710sdmiod.apps.googleusercontent.com"
         onLoginStart={onLoginStart}
-        // onResolve={({ provider, data }: IResolveParams) => {
-        //   setProvider(provider);
-        //   setProfile(data);
-        // }}
+        onResolve={({ provider, data }: IResolveParams) => {
+          setProvider(provider);
+          setProfile(data);
+        }}
         onReject={(err: any) => {
           console.log(err);
         }}
       >
         <GoogleLoginButton />
       </LoginSocialGoogle> */}
+      {/* <GoogleLoginButton onClick={googleLogIn}>구글 로그인</GoogleLoginButton> */}
     </Form>
   );
 }
