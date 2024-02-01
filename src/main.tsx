96256560ee1739f1d0f68b0a7e22c804926342c7
page1597 <62283847+page1597@users.noreply.github.com>
2024-02-02 00:53:10 +0900

    #20 로그아웃 시 장바구니 초기화 안되는 문제 해결
    장바구니 상태 Provider를 인증 상태 Provider보다 상위에 위치시킴

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import AuthProvider from "./provider/authProvider.tsx";
 import BasketProvider from "./provider/basketProvider.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BasketProvider>
+    <BasketProvider>
+      <AuthProvider>
         <App />
-      </BasketProvider>
-    </AuthProvider>
+      </AuthProvider>
+    </BasketProvider>
   </React.StrictMode>
 );
