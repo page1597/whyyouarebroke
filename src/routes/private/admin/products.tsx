ab25b0f1c3207d23509a70724e198c5a2c0830e3
page1597 <62283847+page1597@users.noreply.github.com>
2024-02-03 15:21:17 +0900

    판매자 메인 페이지 상품 목록에 정렬 필터 추가

--- src/routes/private/admin/products.tsx
+++ src/routes/private/admin/products.tsx
@@ -1,16 +1,21 @@
 import { getProducts } from "@/services/firebase";
 import { DocumentData } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
-import { useInfiniteQuery, QueryClient, useMutation } from "@tanstack/react-query";
+import { useInfiniteQuery, QueryClient } from "@tanstack/react-query";
 
 export default function Products() {
   // 판매상품 리스트 목록
   const navigate = useNavigate();
-  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } = useInfiniteQuery({
-    queryKey: ["products"],
-    queryFn: ({ pageParam }) => getProducts(12, pageParam),
+  const [orderby, setOrderby] = useState<string>("createdAt");
+  const [inViewRef, inView] = useInView({
+    triggerOnce: false,
+  });
+
+  const { data, hasNextPage, isFetchingNextPage, fetchNextPage, refetch } = useInfiniteQuery({
+    queryKey: ["products", orderby],
+    queryFn: ({ pageParam }) => getProducts(orderby, 12, pageParam),
     initialPageParam: null,
     getNextPageParam: (querySnapshot) => {
       if (querySnapshot.length < 12) {
@@ -29,25 +34,47 @@ export default function Products() {
   });
 
   async function prefetchNextPage() {
-    console.log("hasNextPage", hasNextPage, "isFetchingNextPage", isFetchingNextPage);
     if (hasNextPage && !isFetchingNextPage) {
       await queryClient.prefetchInfiniteQuery({ queryKey: ["products"], queryFn: fetchNextPage, pages: 3 });
     }
   }
 
-  const [inViewRef, inView] = useInView({
-    triggerOnce: false,
-  });
   useEffect(() => {
     if (inView) {
       prefetchNextPage();
     }
-  }, [inView]);
+  }, [inView, orderby]);
+
+  const changeOrderby = (order: string) => {
+    console.log(order);
+    setOrderby(order);
+    refetch();
+  };
 
   return (
     <>
-      <h3 className="text-xl">전체 상품</h3>
-
+      {" "}
+      <div className="flex flex-row justify-between items-end">
+        <h3 className="text-xl">전체 상품</h3>
+        <div className="flex gap-3">
+          <button
+            name="createdAt"
+            value={orderby}
+            onClick={() => changeOrderby("createdAt")}
+            className={`bg-transparent text-zinc-600 hover:bg-transparent text-sm ${orderby === "createdAt" ? "font-bold" : "font-medium"}`}
+          >
+            최신순
+          </button>
+          <button
+            name="price"
+            value={orderby}
+            onClick={() => changeOrderby("price")}
+            className={`bg-transparent text-zinc-600 hover:bg-transparent text-sm ${orderby === "price" ? "font-extrabold" : "font-medium"}`}
+          >
+            가격순
+          </button>
+        </div>
+      </div>
       <button
         title="상품 등록"
         className="fixed right-10 bottom-10 bg-zinc-200 rounded-full p-2 z-10"
@@ -69,7 +96,6 @@ export default function Products() {
           <line x1="5" y1="12" x2="19" y2="12"></line>
         </svg>
       </button>
-
       <div className="mt-8">
         {data?.pages.map((page, index) => (
           <div key={index}>
@@ -109,7 +135,6 @@ export default function Products() {
           </div>
         ))}
       </div>
-
       <div ref={inViewRef} className="h-42 w-full">
         {isFetchingNextPage && <p>loading...</p>}
       </div>
