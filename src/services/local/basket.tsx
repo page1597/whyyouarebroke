9f4ba99486a1e40468e06c5f3733debc1d7b1c43
page1597 <62283847+page1597@users.noreply.github.com>
2024-02-13 16:04:41 +0900

    Fix: close #46 비회원(로그아웃) 상태에서 페이지 렌더링 시마다 장바구니가 초기화되는 문제
    - 장바구니를 담는 기능을 수행할 때 로컬에 현재 로그인 상태를 함께 넣어줌
    - 로그아웃 상태에서 장바구니를 담은 경우 다른 페이지 이동 시에도 장바구니가 삭제(초기화)되지 않음.
    - 비회원 장바구니 저장소 localStorage에서 sessionStorage로 변경

--- src/services/local/basket.tsx
+++ src/services/local/basket.tsx
@@ -5,19 +5,18 @@ import { fbGetUser } from "../firebase/user";
 // 로컬 스토리지 장바구니만 가져오기
 export function getBasket() {
   console.log("get basket");
-  let localStorageBasket = localStorage.getItem("basket");
-  let localBasket = localStorageBasket === null ? [] : JSON.parse(localStorageBasket);
+  let sessionStorageBasket = sessionStorage.getItem("basket");
+  let localBasket = sessionStorageBasket === null ? [] : JSON.parse(sessionStorageBasket);
   return localBasket;
 }
 
-// 장바구니에 들어있는 상품인지 확인
-export function isInBasket(productId: string) {
-  console.log("isInBasket");
-
-  let getBasket = localStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
+// 장바구니에 들어있는 상품인지 & 같은 수량으로 들어 있는지 확인
+export function hasSameQuantityInBasket(productId: string, quantity: number) {
+  let getBasket = sessionStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
   let basket = getBasket === null ? [] : JSON.parse(getBasket); // 원래 장바구니 리스트
-  const existingProductIndex = basket.findIndex((item: { id: string }) => item.id === productId);
-  if (existingProductIndex !== -1) {
+  const existingProduct = basket.find((item: { id: string; quantity: number }) => item.id === productId);
+
+  if (existingProduct && existingProduct.quantity === quantity) {
     return true;
   }
   return false;
@@ -25,7 +24,7 @@ export function isInBasket(productId: string) {
 
 // 로컬과 firebase DB의 장바구니를 통일
 export async function matchBasketlocalToDB(userId: string) {
-  const getLocalBasket = localStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
+  const getLocalBasket = sessionStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
   const localBasket = getLocalBasket === null ? [] : JSON.parse(getLocalBasket); // 원래 장바구니 리스트
 
   const userData = await fbGetUser(userId);
@@ -41,12 +40,12 @@ export async function matchBasketlocalToDB(userId: string) {
   });
 
   fbUpdateUserBasket(userId, uniqueBasket);
-  localStorage.setItem("basket", JSON.stringify(uniqueBasket));
+  sessionStorage.setItem("basket", JSON.stringify(uniqueBasket));
 }
 
 // 장바구니에 추가
 export function addToBasket(userId: string | null, product: ProductType, quantity: number) {
-  let getBasket = localStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
+  let getBasket = sessionStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
   let basket = getBasket === null ? [] : JSON.parse(getBasket); // 원래 장바구니 리스트
   // 상품이 이미 장바구니에 있는지 확인
   const existingProductIndex = basket.findIndex((item: { id: string }) => item.id === product.id);
@@ -68,7 +67,8 @@ export function addToBasket(userId: string | null, product: ProductType, quantit
     basket.push(newBasket);
   }
 
-  localStorage.setItem("basket", JSON.stringify(basket));
+  sessionStorage.setItem("basket", JSON.stringify(basket));
+  sessionStorage.setItem("loggedIn", userId ? "yes" : "no");
 
   if (userId) {
     // 로그인 한 상태라면
@@ -78,7 +78,7 @@ export function addToBasket(userId: string | null, product: ProductType, quantit
 }
 
 export function updateBasketProductStock(userId: string | null, basketProduct: BasketProductType, stock: number) {
-  let getBasket = localStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
+  let getBasket = sessionStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
   let basket = getBasket === null ? [] : JSON.parse(getBasket); // 원래 장바구니 리스트
 
   // 상품이 이미 장바구니에 있는지 확인
@@ -89,7 +89,7 @@ export function updateBasketProductStock(userId: string | null, basketProduct: B
     basket[existingProductIndex].stock = stock;
   }
 
-  localStorage.setItem("basket", JSON.stringify(basket));
+  sessionStorage.setItem("basket", JSON.stringify(basket));
 
   if (userId) {
     // 로그인 한 상태라면
@@ -100,7 +100,7 @@ export function updateBasketProductStock(userId: string | null, basketProduct: B
 
 // 수량 조절은 불가
 export function removeFromBasket(userId: string | null, productId: string) {
-  let getBasket = localStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
+  let getBasket = sessionStorage.getItem("basket"); // 원래 있던 장바구니 꺼내기
   let basket = getBasket === null ? [] : JSON.parse(getBasket); // 원래 장바구니 리스트
 
   // 지울 상품 찾기
@@ -109,7 +109,7 @@ export function removeFromBasket(userId: string | null, productId: string) {
   if (productIndex !== -1) {
     basket.splice(productIndex, 1);
 
-    localStorage.setItem("basket", JSON.stringify(basket));
+    sessionStorage.setItem("basket", JSON.stringify(basket));
 
     if (userId) {
       // 로그인 한 상태라면 DB에도 업데이트
