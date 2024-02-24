9268db76251b3ade1bc9527a022fc3de18707b9e
page1597 <62283847+page1597@users.noreply.github.com>
2024-02-24 15:10:11 +0900

    Test: 회원가입 폼 테스트코드 작성

--- src/components/__test__/signUpForm.test.jsx
+++ src/components/__test__/signUpForm.test.jsx
@@ -2,7 +2,7 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import SignUpForm from "../signUpForm";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router } from "react-router-dom"; // BrowserRouter를 import 해야합니다.
-import { FormProvider } from "react-hook-form";
+import userEvent from "@testing-library/user-event";
 // import { FormProvider, Form } from "react-hook-form";
 window.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
@@ -10,45 +10,32 @@ window.ResizeObserver = jest.fn().mockImplementation(() => ({
   disconnect: jest.fn(),
 }));
 
-const mockSignUp = jest.fn((email, password, passwordConfirm, name) => {
-  return Promise.resolve({ email, password, passwordConfirm, name });
+const mockSignUp = jest.fn((email, password) => {
+  return Promise.resolve({ email, password });
 });
-
 describe("signUpForm 테스트", () => {
   test("회원가입 버튼을 클릭했을 때 폼 제출이 제대로 되는지 확인", async () => {
     const queryClient = new QueryClient();
     render(
       <Router>
         <QueryClientProvider client={queryClient}>
-          {/* <FormProvider> */}
           <SignUpForm signUp={mockSignUp} />
-          {/* </FormProvider> */}
         </QueryClientProvider>
       </Router>
     );
-    fireEvent.input(screen.getByLabelText("이메일 *"), {
-      target: {
-        value: "fake@example.com",
-      },
-    });
-    // fireEvent.input(screen.getByLabelText("비밀번호 *"), {
-    //   target: {
-    //     value: "iamfakepw!",
-    //   },
-    // });
-    // fireEvent.input(screen.getByLabelText("비밀번호 확인 *"), {
-    //   target: {
-    //     value: "iamfakepw!",
-    //   },
-    // });
-    // fireEvent.input(screen.getByLabelText("이름 *"), {
-    //   target: {
-    //     value: "가짜닉네임",
-    //   },
-    // });
+
+    // await userEvent.type(screen.getByLabelText("이메일 *"), "fake@example.com", { allAtOnce: true });
+    // await userEvent.type(screen.getByLabelText("비밀번호 *"), "qlalf!234", { allAtOnce: true });
+    const emailInput = screen.getByLabelText("이메일 *");
+    const passwordInput = screen.getByLabelText("비밀번호 *");
+
+    await form.control.setValue("email", "fake@example.com");
+    await form.control.setValue("password", "qlalf!234");
+    expect(emailInput.value).toBe("fake@example.com");
+    expect(passwordInput.value).toBe("qlalf!234");
+
     fireEvent.submit(screen.getByRole("button"));
-    await waitFor(() => expect(screen.queryAllByRole("alert")).toHaveLength(0));
-    expect(mockSignUp).toHaveBeenCalledWith("fake@example.com");
-    // expect(await screen.findAllByDisplayValue("비밀번호가 다릅니다.")).toBe();
+
+    await waitFor(() => expect(mockSignUp).toHaveBeenCalledWith("fake@example.com", "qlalf!234"));
   });
 });
