b4a97a7d995f4e66e3f56756135727b199a10316
page1597 <62283847+page1597@users.noreply.github.com>
2024-03-18 11:36:32 +0900

    Optimize: React.memo 적용

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -1,7 +1,8 @@
+import { memo } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "src/assets/logo.webp";
 
-export default function Logo({ width, height, className }: { width: number; height: number; className?: string }) {
+function Logo({ width, height, className }: { width: number; height: number; className?: string }) {
   const navigate = useNavigate();
   return (
     <div onClick={() => navigate("/")} className={`cursor-pointer ${className}`}>
@@ -9,3 +10,4 @@ export default function Logo({ width, height, className }: { width: number; heig
     </div>
   );
 }
+export default memo(Logo);
